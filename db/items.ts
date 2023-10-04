c325e5f519f6152eacf11b2283591f1eceaf084f
theUniParse <theUniParse@gmail.com>
2023-10-04 03:10:55 +0100

    improve db simulation

--- db/items.ts
+++ db/items.ts
@@ -3,13 +3,14 @@ import path from 'path'
 
 const itemsPath = path.join(process.cwd(), 'db', 'items.json')
 
-interface Item {
+export interface Item {
   name: string
   price: number
-  quantity: number
+  stock: number
+  image: string | null
 }
 
-interface Items {
+export interface Items {
   [id: string]: Item
 }
 
@@ -19,56 +20,56 @@ export function getItems() {
   return items as Items
 }
 
-export function getItem(id: string) {
+export function getItem({ id }: { id: string }) {
   const items = getItems()
   const item = items[id]
   return item
 }
 
-export function hasItem(id: string) {
+export function hasItem({ id }: { id: string }) {
   const items = getItems()
   const item = items[id]
-  if (!item) return false
-  const availability = item.quantity
+  const availability = !!item?.stock
   return availability
 }
 
-export function addItem(item: Item) {
+export function insertItem(item: Item) {
   const items = getItems()
   const id = crypto.randomUUID()
   items[id] = item
   updateItems(items)
 }
 
-export function setItem(id: string, quantity: number) {
-  const items = getItems()
-  const item = items[id]
-
-  if (!item) throw new Error(`undefined item with id: ${id}`)
-
-  item.quantity = quantity
-  updateItems(items)
-}
-
-export function adjustItem(id: string, quantity: number) {
-  const items = getItems()
-  const item = items[id]
-
-  if (!item) throw new Error(`undefined item with id: ${id}`)
-
-  const prevQuantity = item.quantity
-  item.quantity += quantity
-
-  if (item.quantity < 0)
-    throw Error(`can't decrease more than ${prevQuantity}`)
-
-  updateItems(items)
-}
-
-export function deleteItem(id: string) {
+export function deleteItem({ id }: { id: string }) {
   const items = getItems()
   delete items[id]
+  updateItems(items)
+}
+export interface Order {
+  id: string
+  quantity: number
+}
 
+export function setItemStock({ id, quantity }: Order) {
+  const items = getItems()
+
+  const item = items[id]
+  if (!item) throw new Error(`undefined item with id: ${id}`)
+
+  item.stock = quantity
+  updateItems(items)
+}
+
+export function adjustItemStock({ id, quantity }: Order) {
+  const items = getItems()
+
+  const item = items[id]
+  if (!item) throw new Error(`undefined item with id: ${id}`)
+
+  if (item.stock < quantity)
+    throw Error(`can't decrease more than ${item.stock}`)
+
+  item.stock += quantity
   updateItems(items)
 }
 
